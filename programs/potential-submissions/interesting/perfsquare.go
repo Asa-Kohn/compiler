28dd44d5fafd3a6c3a4327b897c7482ff1c4d212
Alexander Iannantuono <a.iannantuono@gmail.com>
2020-02-18 13:26:41 -0500

    added interesting (hopefully) valid programs

---
+++ programs/potential-submissions/interesting/perfsquare.go
@@ -0,0 +1,23 @@
+/* Checks if a number is a perfect square using elementary properties.
+ * \sum_{i=0}^n (2i + 1) = (n + 1)^2
+ */
+
+package main
+
+func perf(num int) bool {
+    // Start removing from the input
+    for i := 0; num > 0; i++ {
+        num -= 2  * i + 1
+    }
+
+    if(num == 0) {
+        return true;
+    }
+
+    return false;
+}
+
+func main() {
+	var num int = 20;
+    println("It is", perf(num), "that", num, "is a perfect square.");
+}
