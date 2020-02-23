001f25a79da9658f7afd56619fab47fdf45e5ecf
Po Wei CHIEN <pchien@teaching.CS.McGill.CA>
2020-02-22 20:26:35 -0500

    added return values

--- src/weed.c
+++ src/weed.c
@@ -4,21 +4,21 @@
 #include "weed.h"
 
 //stmts_kind
-void weed_stmts(STMTS *stmts) {
+int weed_stmts(STMTS *stmts) {
     if(stmts == NULL) return 0;
-    weed_stmts(stmts->next);
-    weed_stmt(&(stmts->stmt));
+    return weed_stmts(stmts->next) || 
+        weed_stmt(&(stmts->stmt));
 }
 
 //stmt_kind
-void weed_stmt(STMT *stmt) {
+int weed_stmt(STMT *stmt) {
     if(stmt == NULL) return 0;
     switch (stmt->kind) {
         case tree_stmt_kind_exp:
         case tree_stmt_kind_assign:
-            break;
+            return 0;
         case tree_stmt_kind_shortdecl:
-            weed_shortdecl(&(stmt->shortdecl));
+            return weed_shortdecl(&(stmt->shortdecl));
         case tree_stmt_kind_inc:
         case tree_stmt_kind_dec:
         case tree_stmt_kind_var_decl:
@@ -41,59 +41,61 @@ void weed_stmt(STMT *stmt) {
         case tree_stmt_kind_fallthrough:
             return 0;
     }
+    return 0;
 }
 
 //decls_kind
-void weed_decls(DECLS *decls) {
+int weed_decls(DECLS *decls) {
     if(decls == NULL) return 0;
     switch (decls->kind) {
-        //TODO
         case tree_decls_kind_var_decl:
-            weed_var_decl(decls->var_spec);
+            return weed_var_spec(decls->var_spec);
         case tree_decls_kind_type_spec:
-            weed_type_spec(&(decls->type_spec));
+            return weed_type_spec(&(decls->type_spec));
         case tree_decls_kind_func_decl:
-            weed_func_decl(&(decls->func_decl));
+            return weed_func_decl(&(decls->func_decl));
     }
-    weed_decls(decls->next);
+    return weed_decls(decls->next);
 }
 
-void weed_vars(VARS *vars){
+int weed_vars(VARS *vars){
     if(vars == NULL) return 0;
-    weed_type(vars->type);
-    weed_vars(vars->next);
+    return weed_type(vars->type) ||
+        weed_vars(vars->next);
 }
 
-void weed_var_spec(VAR_SPEC *var_spec) {
+int weed_var_spec(VAR_SPEC *var_spec) {
     if(var_spec == NULL) return 0;
-    weed_type(var_spec->type);
-    weed_exp(var_spec->val);
-    weed_var_spec(var_spec->next);
+    return weed_type(var_spec->type) ||
+        weed_exp(var_spec->val) ||
+        weed_var_spec(var_spec->next);
 }
 
 //TODO
-void weed_type_spec(TYPE_SPEC *type_spec);
+int weed_type_spec(TYPE_SPEC *type_spec);
 
-void weed_func_decl(FUNC_DECL *func_decl){
+int weed_func_decl(FUNC_DECL *func_decl){
     if(func_decl == NULL) return 0;
-    weed_vars(func_decl->params);
-    weed_type(func_decl->type);
-    weed_stmts(func_decl->body);
+    return weed_vars(func_decl->params) ||
+        weed_type(func_decl->type) ||
+        weed_stmts(func_decl->body);
 }
 
 //type_kind
-void weed_type(TYPE *type) {
+int weed_type(TYPE *type) {
     if(type == NULL) return 0;
     switch (type->kind) {
         case tree_type_kind_name:
         case tree_type_kind_array:
         case tree_type_kind_slice:
         case tree_type_kind_struct:
+            return 0;
     }
+    return 0;
 }
 
 //binaryexp_kind
-void weed_binaryexp(BINARYEXP *binaryexp){
+int weed_binaryexp(BINARYEXP *binaryexp){
     if(binaryexp == NULL) return 0;
     switch (binaryexp->kind) {
         case tree_binaryexp_kind_or:
@@ -115,19 +117,20 @@ void weed_binaryexp(BINARYEXP *binaryexp){
         case tree_binaryexp_kind_rshift:
         case tree_binaryexp_kind_bitand:
         case tree_binaryexp_kind_andnot:
-            weed_exp(binaryexp->left);
-            weed_exp(binaryexp->right);
+            return weed_exp(binaryexp->left) ||
+                weed_exp(binaryexp->right);
     }
+    return 0;
 }
 
-void weed_exps(EXPS *exps) {
+int weed_exps(EXPS *exps) {
     if(exps == NULL) return 0;
-    weed_exp(exps->exp);
-    weed_exps(exps->next);
+    return weed_exp(exps->exp) ||
+        weed_exps(exps->next);
 }
 
 //exp_kind
-void weed_exp(EXP *exp) {
+int weed_exp(EXP *exp) {
     if(exp == NULL) return 0;
     switch (exp->kind) {
         case tree_exp_kind_ident:
@@ -148,98 +151,104 @@ void weed_exp(EXP *exp) {
         case tree_exp_kind_append:
         case tree_exp_kind_len:
         case tree_exp_kind_cap:
+            return 0;
     }
+    return 0;
 }
 
 //unaryexp_kind
-void weed_unaryexp(UNARYEXP *unarexp) {
+int weed_unaryexp(UNARYEXP *unarexp) {
     if(unarexp == NULL) return 0;
     switch (unarexp->kind) {
         case tree_unaryexp_kind_plus:
         case tree_unaryexp_kind_minus:
         case tree_unaryexp_kind_not:
         case tree_unaryexp_kind_comp:
+            return 0;
     }
+    return 0;
 }
 
 
-void weed_type_array(TYPE_ARRAY *type_array) {
+int weed_type_array(TYPE_ARRAY *type_array) {
     if(type_array == NULL) return 0;
-    weed_type(type_array->type);
+    return weed_type(type_array->type);
 }
 
-void weed_type_slice(TYPE_SLICE *type_slice) {
+int weed_type_slice(TYPE_SLICE *type_slice) {
     if(type_slice == NULL) return 0;
-    weed_type(type_slice->type);
+    return weed_type(type_slice->type);
 }
 
-void weed_type_struct(TYPE_STRUCT *type_struct) {
+int weed_type_struct(TYPE_STRUCT *type_struct) {
     if(type_struct == NULL) return 0;
-    weed_vars(type_struct->fields);
+    return weed_vars(type_struct->fields);
 }
 
-void weed_call(CALL *call) {
+int weed_call(CALL *call) {
     if(call == NULL) return 0;
-    weed_exp(call->func);
-    weed_exps(call->exps);
+    return weed_exp(call->func) ||
+        weed_exps(call->exps);
 }
 
-void weed_index(INDEX *index) {
+int weed_index(INDEX *index) {
     if(index == NULL) return 0;
-    weed_exp(index->arr);
-    weed_exp(index->index);
+    return weed_exp(index->arr) ||
+        weed_exp(index->index);
 }
 
-void weed_field(FIELD *field) {
+int weed_field(FIELD *field) {
     if(field == NULL) return 0;
-    weed_exp(field->instance);
+    return weed_exp(field->instance);
 }
 
-void weed_append(APPEND *append){
+int weed_append(APPEND *append){
     if(append == NULL) return 0;
-    weed_exp(append->exp1);
-    weed_exp(append->exp2);
+    return weed_exp(append->exp1) ||
+        weed_exp(append->exp2);
 }
 
-void weed_cases(CASES *cases) {
+int weed_cases(CASES *cases) {
     if(cases == NULL) return 0;
-    weed_exp(cases->val);
-    weed_stmts(cases->body);
-    weed_cases(cases->next);
+    return weed_exp(cases->val) ||
+        weed_stmts(cases->body) ||
+        weed_cases(cases->next);
 }
 
-void weed_assign(ASSIGN *assign) {
+int weed_assign(ASSIGN *assign) {
     if(assign == NULL) return 0;
-    weed_exp(assign->exp);
+    return weed_exp(assign->exp);
 }
 
-void weed_shortdecl(SHORTDECL *shortdecl) {
+int weed_shortdecl(SHORTDECL *shortdecl) {
     if(shortdecl == NULL) return 0;
-        // fprintf(stderr, "Error: short declaration (line%d)", exp->lineno);
-        // exit(1);
-        return 1;
+    // fprintf(stderr, "Error: short declaration (line%d)", exp->lineno);
+    // exit(1);
+    return 1;
     // weed_exp(shortdecl->exp);
 }
 
-void weed_if(IF_STMT *if_stmt) {
+int weed_if(IF_STMT *if_stmt) {
     if(if_stmt == NULL) return 0;
-    weed_stmts(if_stmt->init);
-    weed_exp(if_stmt->condition);
-    weed_stmts(if_stmt->body);
-    weed_stmts(if_stmt->elsebody);
+    return weed_stmts(if_stmt->init) ||
+        weed_exp(if_stmt->condition) ||
+        weed_stmts(if_stmt->body) ||
+        weed_stmts(if_stmt->elsebody);
 }
 
-void weed_switch(SWITCH_STMT *switch_stmt) {
+int weed_switch(SWITCH_STMT *switch_stmt) {
     if(switch_stmt == NULL) return 0;
-    weed_stmts(switch_stmt->init);
-    weed_exp(switch_stmt->exp);
-    weed_cases(switch_stmt->cases);
+    //switch statements not supported
+    return 1;
+        // weed_stmts(switch_stmt->init) ||
+        // weed_exp(switch_stmt->exp) ||
+        // weed_cases(switch_stmt->cases);
 }
 
-void weed_for(FOR_STMT *for_stmt) {
+int weed_for(FOR_STMT *for_stmt) {
     if(for_stmt == NULL) return 0;
-    weed_stmts(for_stmt->init);
-    weed_exp(for_stmt->condition);
-    weed_stmts(for_stmt->iter);
-    weed_stmts(for_stmt->body);
+    return weed_stmts(for_stmt->init) ||
+        weed_exp(for_stmt->condition) ||
+        weed_stmts(for_stmt->iter) ||
+        weed_stmts(for_stmt->body);
 }
