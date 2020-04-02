e762efc1303601011a53d26e4b0dc49afd1eaf42
Po Wei CHIEN <pchien@teaching.CS.McGill.CA>
2020-04-02 02:07:52 -0400

    test script

---
+++ programs/testscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+for folder in `ls all-testers/M2`
+do
+	echo "Entering folder ${folder}"
+
+	cd "all-testers/M2/${folder}"
+	for file in `ls`
+	do
+		# echo "${file}"
+		echo "Testing ${file} on winnipeg"
+		../../../../src/winnipeg typecheck < "${file}"
+	done
+	cd ..
+	cd ..
+	cd ..
+
+	echo " "
+done
