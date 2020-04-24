03d8c7e0eac0361a8f79ad75d93968d97958aed3
Alexander Iannantuono <a.iannantuono@gmail.com>
2020-04-24 16:57:53 -0400

    add printing, folders no longer needed

--- programs/all-testers/M4/testers/printing/int.go
+++ programs/all-testers/M4/testers/printing/int.go
@@ -3,5 +3,9 @@
 package main
 
 func main() {
-	
+	println(1)
+	println(2)
+	println(3)
+	println(4)
+	println(5)
 }
---
+++ programs/all-testers/M4/testers/printing/together.go
@@ -0,0 +1,7 @@
+// generated from template.go
+
+package main
+
+func main() {
+	println(1, True, 2, False, 3, 1., 4, 2., -1, -1., -0.1 , "-1")
+}
